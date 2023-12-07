276e1e095d8099f82bd6003c083de129a17ee478
이상훈 <slee@i-magicsoft.com>
2023-12-07 14:22:30 +0900

    /resources/static에 있는 파일들은 인증하지 않고 모두 허용

--- src/main/java/com/studcafe/config/SecurityConfig.java
+++ src/main/java/com/studcafe/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.studcafe.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -32,5 +34,11 @@ public class SecurityConfig {
         );
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
