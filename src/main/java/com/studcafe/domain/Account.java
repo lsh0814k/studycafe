15984ff8ab2a10037057e146642a41e7a45310a2
이상훈 <slee@i-magicsoft.com>
2023-12-07 10:33:56 +0900

    주석 추가

--- src/main/java/com/studcafe/domain/Account.java
+++ src/main/java/com/studcafe/domain/Account.java
@@ -11,34 +11,38 @@ import static lombok.AccessLevel.*;
 @Entity @Getter @EqualsAndHashCode(of ="id")
 @Builder @AllArgsConstructor @NoArgsConstructor(access = PROTECTED)
 public class Account {
+
     @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
+    // ---- 로그인 ----
+    @Column(unique = true) // 로그인 시 유니크 해야 하는 필드
     private String email;
 
-    @Column(unique = true)
+    @Column(unique = true) // 로그인 시 유니크 해야 하는 필드
     private String nickname;
 
     private String password;
 
-    private boolean emailVerified;
+    private boolean emailVerified; // 이메일 인증 절차(이메일 인증 여부를 검토)
 
-    private String emailCheckToken;
+    private String emailCheckToken; // 이메일 검증 시 필요한 토큰
 
-    private LocalDateTime joinedAt;
+    private LocalDateTime joinedAt; // 검증 후 가입 날짜 설정
 
-    private String bio;
+    // ---- 프로필 ----
+    private String bio; // 짧은 자기소개
 
-    private String url;
+    private String url; // 웹 사이트 url
 
-    private String occupation;
+    private String occupation; // 직업
 
-    private String location;
+    private String location; // 지역
 
     @Lob @Basic(fetch = EAGER)
-    private String profileImage;
+    private String profileImage; // 프로필 이미지
 
+    // ---- 알림 설정 ----
     private boolean studyCreatedByEmail;
 
     private boolean studyCreatedByWeb;
