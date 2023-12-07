0a02c2df6e3ddb2e523abd4683df0de1ff714a14
이상훈 <slee@i-magicsoft.com>
2023-12-07 12:33:57 +0900

    회원가입 컨트롤러

---
+++ src/main/java/com/studcafe/account/AccountController.java
@@ -0,0 +1,20 @@
+package com.studcafe.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String joinForm(Model model) {
+        // 타임리프가 의존성 주입이 되어 있다면
+        // org.springframework.boot.autoconfigure.thymeleaf가 자동 설정 된다.
+        // spring:
+        //  thymeleaf:
+        //    prefix: classpath:/templates/
+        //    suffix: .html
+        return "account/sign-up";
+    }
+}
---
+++ src/test/java/com/studcafe/account/AccountControllerTest.java
@@ -0,0 +1,38 @@
+package com.studcafe.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * security가 적용 되어 있으면 sign-up 요청이 왔을 때
+     * AccessDeniedException이 발생한다.
+     * 이때 security Filter가 해당 exception을 catch해
+     * form generation과 함께 /login으로 rediredt 시킨다.
+     */
+    @Test
+    @DisplayName("회원 가입 화면 보이는지 테스트")
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+
+}
