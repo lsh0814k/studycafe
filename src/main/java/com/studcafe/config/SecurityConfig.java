3fb33bd12af1167d07f6f7205be7187ce390b5ed
이상훈 <slee@i-magicsoft.com>
2023-12-07 12:34:31 +0900

    security 설정 * 특정 url은 인증하지 않도록 설정

---
+++ src/main/java/com/studcafe/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.studcafe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        // 특정 요청들을 authorize 체크를 하지 않도록 설정할 수 있다.
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers(
+                        mvcMatcherBuilder.pattern("/"),
+                        mvcMatcherBuilder.pattern("/login"),
+                        mvcMatcherBuilder.pattern("/sign-up"),
+                        mvcMatcherBuilder.pattern("/check-email"),
+                        mvcMatcherBuilder.pattern("/check-email-token"),
+                        mvcMatcherBuilder.pattern("/email-login"),
+                        mvcMatcherBuilder.pattern("/check-email-login"),
+                        mvcMatcherBuilder.pattern("/login-link"),
+                        mvcMatcherBuilder.pattern(HttpMethod.GET, "/profile/*")
+                ).permitAll() // 해당 요청들은 모두 허용한다.
+                .anyRequest().authenticated() // 그 외 요청들은 모두 인증해야 한다.
+        );
+
+        return http.build();
+    }
+}
