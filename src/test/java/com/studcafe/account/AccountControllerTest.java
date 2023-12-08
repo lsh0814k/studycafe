e985f357537ce7f0e1a7eba406385542131de2fd
이상훈 <slee@i-magicsoft.com>
2023-12-08 16:54:30 +0900

    accountController 테스트 코드 작성

--- src/test/java/com/studcafe/account/AccountControllerTest.java
+++ src/test/java/com/studcafe/account/AccountControllerTest.java
@@ -1,13 +1,23 @@
 package com.studcafe.account;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -16,6 +26,17 @@ class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
+
+    @BeforeEach
+    public void init() {
+        accountRepository.deleteAll();
+    }
 
     /**
      * security가 적용 되어 있으면 sign-up 요청이 왔을 때
@@ -32,5 +53,58 @@ class AccountControllerTest {
                 .andExpect(model().attributeExists("signUpForm"));
     }
 
+    @Test
+    @DisplayName("회원 가입 처리 - csrf 값이 없어 403 에러 발생")
+    void signUpSubmit_no_csrf() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("nickname", "nick")
+                .param("email", "email..")
+                .param("password", "12345")
+        )
+        .andExpect(status().isForbidden());
+    }
 
+    @Test
+    @DisplayName("회원 가입 처리 - 입력 값 오류")
+    void signUpSubmit_with_wrong_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .with(csrf())
+                        .param("nickname", "nick")
+                        .param("email", "email..")
+                        .param("password", "12345")
+                )
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @Test
+    @DisplayName("회원 가입 처리 - 입력 값 정상")
+    void signUp_with_correct_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .with(csrf())
+                        .param("nickname", "nick")
+                        .param("email", "email@naver.com")
+                        .param("password", "a151385wa3!")
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        assertTrue(accountRepository.existsByEmail("email@naver.com"));
+        assertTrue(accountRepository.existsByNickname("nick"));
+    }
+
+    @Test
+    @DisplayName("회원 가입 처리 - 입력 값 정상(메일 발송 여부)")
+    void signUp_with_correct_input_send_mail() throws Exception{
+        mockMvc.perform(post("/sign-up")
+                        .with(csrf())
+                        .param("nickname", "nick")
+                        .param("email", "email@naver.com")
+                        .param("password", "a151385wa3!")
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
+    }
 }
