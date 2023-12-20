509a77907c9fb0ee17a0894d552e1c79e57bee73
lee <lsh0814k@gmail.com>
2023-12-20 00:25:49 -0800

    Account를 조회할 때 Tag 정보도 같이 가져오도록 @EntityGraph 추가 추후 변경 예정

--- src/main/java/com/studcafe/account/repository/AccountRepository.java
+++ src/main/java/com/studcafe/account/repository/AccountRepository.java
@@ -1,18 +1,24 @@
 package com.studcafe.account.repository;
 
 import com.studcafe.account.domain.Account;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional(readOnly = true)
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
 
+    @EntityGraph(attributePaths = {"tags"})
     Optional<Account> findByEmail(String email);
 
     Optional<Account> findByNickname(String nickname);
+
+    @EntityGraph(attributePaths = {"tags"})
+    Optional<Account> findWithTagsByNickname(String nick);
 }
