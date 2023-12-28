365a0bb910cace5b99bfac350ae4d6360328d472
이상훈 <slee@i-magicsoft.com>
2023-12-28 16:33:10 +0900

    알림 도메인

---
+++ src/main/java/com/studycafe/modules/notification/domain/Notification.java
@@ -0,0 +1,37 @@
+package com.studycafe.modules.notification.domain;
+
+import com.studycafe.modules.account.domain.Account;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.FetchType.*;
+import static lombok.AccessLevel.*;
+
+@Entity
+@Getter @NoArgsConstructor(access = PRIVATE) @EqualsAndHashCode(of="id")
+@AllArgsConstructor @Builder
+public class Notification {
+
+    @Id @GeneratedValue
+    @Column(name = "notification_id")
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private Boolean checked;
+
+    @ManyToOne(fetch = LAZY)
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(value = STRING)
+    private NotificationType notificationType;
+}
---
+++ src/main/java/com/studycafe/modules/notification/domain/NotificationType.java
@@ -0,0 +1,5 @@
+package com.studycafe.modules.notification.domain;
+
+public enum NotificationType {
+    STUDY_CREATED, STUDY_UPDATED, EVENT_ENROLLMENT
+}
